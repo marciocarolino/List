37b37f385403850c47759548e80ee0054fb589d2
Marcio Carolino <marciocarolino@gmail.com>
2023-08-19 23:57:48 -0300

    feat: resolvendo desafio ListaTarefa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,30 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
+
+        System.out.println("================================");
+        listaTarefas.adicionarTarefa("Tarefa 1");
+        listaTarefas.adicionarTarefa("Tarefa 2");
+        listaTarefas.adicionarTarefa("Tarefa 3");
+
+        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
+
+        System.out.println("=============REMOVENDO===================");
+        listaTarefas.removerTarefa("Tarefa 1");
+
+
+        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
+
+
+        System.out.println("=============OBTER DESCRIÇÕES TAREFAS===================");
+        listaTarefas.obterDescricoesTarefas();
+
+
+        // System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
+
+
     }
 }
---
+++ src/main/java/org/example/Tarefa.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Tarefa {
+
+    //Atributos
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descricao='" + descricao + '\'' +
+                '}';
+    }
+}
