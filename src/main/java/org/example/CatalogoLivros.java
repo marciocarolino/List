06ddfd07f2221398e3906fe16791674e5705d862
Marcio Carolino <marciocarolino@gmail.com>
2023-08-20 11:15:28 -0300

    feat: Catalogo Livros

---
+++ src/main/java/org/example/CatalogoLivros.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.util.*;
+
+public class CatalogoLivros {
+
+    //Atributo
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisaPorIntervaloAnos(int anoInicial, int anoFinal) {
+
+        List<Livro> livorsPorIntervaloAnos = new ArrayList<>();
+
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livorsPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livorsPorIntervaloAnos;
+    }
+
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,30 +3,42 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        ListaTarefas listaTarefas = new ListaTarefas();
-
-        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
-
-        System.out.println("================================");
-        listaTarefas.adicionarTarefa("Tarefa 1");
-        listaTarefas.adicionarTarefa("Tarefa 2");
-        listaTarefas.adicionarTarefa("Tarefa 3");
-
-        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
-
-        System.out.println("=============REMOVENDO===================");
-        listaTarefas.removerTarefa("Tarefa 1");
-
-
-        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
-
-
-        System.out.println("=============OBTER DESCRIÇÕES TAREFAS===================");
-        listaTarefas.obterDescricoesTarefas();
-
-
-        // System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
+//        ListaTarefas listaTarefas = new ListaTarefas();
+//
+//        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
+//
+//        System.out.println("================================");
+//        listaTarefas.adicionarTarefa("Tarefa 1");
+//        listaTarefas.adicionarTarefa("Tarefa 2");
+//        listaTarefas.adicionarTarefa("Tarefa 3");
+//
+//        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
+//
+//        System.out.println("=============REMOVENDO===================");
+//        listaTarefas.removerTarefa("Tarefa 1");
+//
+//
+//        System.out.println("O número total de elementos na lisa é: " + listaTarefas.obterNumeroTotalTarefa());
+//
+//
+//        System.out.println("=============OBTER DESCRIÇÕES TAREFAS===================");
+//        listaTarefas.obterDescricoesTarefas();
 
 
+        //Catalogo de Livros
+
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 2023);
+        catalogoLivros.adicionarLivro("Livro 5", "Autor 5", 1994);
+        catalogoLivros.adicionarLivro("Livro 6", "Autor 6", 1995);
+
+
+       // System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+       // System.out.println(catalogoLivros.pesquisaPorIntervaloAnos(2020, 2023));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
     }
 }
